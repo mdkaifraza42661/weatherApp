18a2356fff23d2eed5e2f26b383fed19bb1939a8
unknown <kaifraza42661@gmail.com>
2022-11-30 16:57:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let weather = {
+    "apiKey": "e370b9580b365eb13a7724fd35481a08",
+    fetchWeather: function(city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apiKey).then((response) => response.json())
+            .then((data) => this.displayWether(data));
+    },
+    displayWether: function(data) {
+        const {name} = data;
+        const {icon, description} = data.weather[0];
+        const {temp, humidity} = data.main;
+        const {speed} = data.wind;
+        console.log(name, icon, description, temp, humidity, speed);
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").setAttribute("src", "http://openweathermap.org/img/wn/" + icon + ".png")
+        console.log("http://openweathermap.org/img/wn/" + icon + ".png");
+    }
+}
