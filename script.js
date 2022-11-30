c1151ce7b588f83ef3be208bc39607fd27b096c9
mdkaifraza42661 <92093334+mdkaifraza42661@users.noreply.github.com>
2022-11-30 17:38:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,24 @@ let weather = {
         const {speed} = data.wind;
         console.log(name, icon, description, temp, humidity, speed);
         document.querySelector(".city").innerText = "Weather in " + name;
-        document.querySelector(".icon").setAttribute("src", "http://openweathermap.org/img/wn/" + icon + ".png")
-        console.log("http://openweathermap.org/img/wn/" + icon + ".png");
+        document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon + ".png";
+        // document.querySelector(".icon").setAttribute("src", "http://openweathermap.org/img/wn/" + icon + ".png")
+        // console.log("http://openweathermap.org/img/wn/" + icon + ".png");
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°c";
+        document.querySelector(".humidity").innerText = "Humidity : " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind : " + speed + "km";
+    },
+    search: function() {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+
     }
 }
+document.querySelector(".search button").addEventListener("click", function() {
+    weather.search()
+})
+document.querySelector(".search-bar").addEventListener("keyup", function(event) {
+    if(event.key == "Enter") {
+        weather.search()
+    }
+})
